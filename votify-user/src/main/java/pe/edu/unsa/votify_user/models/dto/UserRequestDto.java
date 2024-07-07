c9e605d405bcb184e39d3b4b33a830ff69900d56
Gabriel <gmachicaoq@unsa.edu.pe>
2024-07-06 19:53:46 -0500

    Corrigiendo el DTO

--- votify-user/src/main/java/pe/edu/unsa/votify_user/models/dto/UserRequestDto.java
+++ votify-user/src/main/java/pe/edu/unsa/votify_user/models/dto/UserRequestDto.java
@@ -4,10 +4,10 @@ import pe.edu.unsa.votify_user.models.bd.User;
 
 import java.util.Date;
 
-public record UserRequestDto (String id, String user_name,  String role, String user_surname, String email, Date created_t, String dni) {
+public record UserRequestDto (String id, String user_name,  String role, String user_surname, String email, Date created_at, String dni) {
 
     public UserRequestDto(User user){
-        this(user.getUser_id(), user.getUser_name(), user.getRole(), user.getUser_surname(), user.getEmail(), user.getCreate_at(), user.getDni());
+        this(user.get_id(), user.getUser_name(), user.getRole(), user.getUser_surname(), user.getEmail(), user.getCreate_at(), user.getDni());
     }
 
 }
