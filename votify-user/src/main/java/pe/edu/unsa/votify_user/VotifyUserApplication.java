4c9f94b73929e23cfd5795c0bf175f4c54686fc2
Gabriel <gmachicaoq@unsa.edu.pe>
2024-07-09 22:49:54 -0500

    Creando mi DockerFile y probando el despliegue en docker

--- votify-user/src/main/java/pe/edu/unsa/votify_user/VotifyUserApplication.java
+++ votify-user/src/main/java/pe/edu/unsa/votify_user/VotifyUserApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class VotifyUserApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(VotifyUserApplication.class, args);
 	}
-
 }
