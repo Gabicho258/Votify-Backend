8a14b1cb2e8a4806372c9340c4e7f246b38b53fc
Gabriel Antony <gmamanican@unsa.edu.pe>
2024-07-17 22:44:43 -0500

    remove unnecessary models

--- election_service/api/models/index.js
+++ election_service/api/models/index.js
@@ -2,7 +2,5 @@ import Candidate from "./candidate.model.js";
 import ElectionProcess from "./electionProcess.model.js";
 import List from "./list.model.js";
 //Not sure to use
-import Credential from "./credential.model.js";
-import User from "./user.model.js";
 
-export { Candidate, ElectionProcess, List, Credential, User };
+export { Candidate, ElectionProcess, List };
